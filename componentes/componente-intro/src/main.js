9e5085fdc3cf201d7dbdf79cf593f7dd2435e56a
edirlucasi7 <icetylucas@gmail.com>
2021-01-10 20:59:58 -0300

    usando CLI / registrando componentes globalmente e localmente

---
+++ componentes/componente-intro/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Contadores from './Contadores.vue'
+
+const app = createApp(App)
+
+app.component('app-contadores', Contadores)
+
+app.mount('#app')
