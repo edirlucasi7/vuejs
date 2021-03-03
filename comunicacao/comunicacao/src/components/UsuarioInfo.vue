ccd6b79c9251fb346334466b18359e44a879ff2a
edirlucasi7 <icetylucas@gmail.com>
2021-03-02 21:38:05 -0300

    comunicação entre componentes pai/filho props filho/pai emitindo evento

---
+++ comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>O nome do usuário é: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="retornarNome">Retornar Nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            // required: true,
+            // default: function() {
+            //     return Array(10).fill(0).join(', ')
+            // }
+            default: 'Anônimo'
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        }, 
+        retornarNome() {
+            const antigo = this.nome
+            this.nome = 'Pedro'
+            this.$emit('NomeMudou', {
+                novo: this.nome,
+                antigo
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
